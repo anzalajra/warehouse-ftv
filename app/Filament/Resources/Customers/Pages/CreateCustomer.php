9b472fb3d74809ccff05936780477411535f4d5f
Anzala Ajra <dang.anzala@gmail.com>
2026-04-28 15:58:54 +0700

    Fix: Customer create fic

--- app/Filament/Resources/Customers/Pages/CreateCustomer.php
+++ app/Filament/Resources/Customers/Pages/CreateCustomer.php
@@ -3,9 +3,21 @@
 namespace App\Filament\Resources\Customers\Pages;
 
 use App\Filament\Resources\Customers\CustomerResource;
+use App\Models\CustomerCategory;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateCustomer extends CreateRecord
 {
     protected static string $resource = CustomerResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        if (empty($data['customer_category_id'])) {
+            $data['customer_category_id'] = CustomerCategory::where('is_active', true)
+                ->orderBy('id')
+                ->value('id');
+        }
+
+        return $data;
+    }
 }
